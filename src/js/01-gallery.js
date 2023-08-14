affea34e052a9101775e3c5079d35f8599379d32
Sahno007 <turnskin90@gmail.com>
2023-08-14 14:38:28 +0300

    upg

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+};
+
+const renderGallery = (items) => {
+  const galleryMarkup = items.map((item) => createGalleryItem(item)).join('');
+  gallery.innerHTML = galleryMarkup;
+};
+
+renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 
 console.log(galleryItems);
