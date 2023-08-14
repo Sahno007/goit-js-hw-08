7cc860e12f5fb2ea224cfdfb5e206ce5f4c1afc2
Sahno007 <turnskin90@gmail.com>
2023-08-14 15:12:38 +0300

    upg

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const THROTTLE_DELAY = 1000; // максимум один раз на секунду
+const currentTimeStorageKey = 'videoplayer-current-time';
+
+const vimeoPlayerElement = document.getElementById('vimeo-player');
+const vimeoPlayer = new Player(vimeoPlayerElement);
+
+vimeoPlayer.on('timeupdate', throttle(function(data) {
+    const currentTime = data.seconds;
+    localStorage.setItem(currentTimeStorageKey, currentTime);
+}, THROTTLE_DELAY));
+
+// Відновлення відтворення з позиції відео, збереженої в сховищі
+const storedTime = localStorage.getItem(currentTimeStorageKey);
+
+if (storedTime) {
+    vimeoPlayer.setCurrentTime(parseFloat(storedTime));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+// Завантаження збережених даних з локального сховища
+const savedState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+emailInput.value = savedState.email || '';
+messageInput.value = savedState.message || '';
+
+// Оновлення локального сховища даними з форми з використанням throttle
+const updateLocalStorage = throttle(() => {
+  const currentState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(currentState));
+}, 500);
+
+// Збереження даних форми в локальне сховище під час зміни введених даних
+emailInput.addEventListener('input', updateLocalStorage);
+messageInput.addEventListener('input', updateLocalStorage);
+
+// Очищення даних форми та локального сховища при відправці
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const currentState = {
+    email: '',
+    message: '',
+  };
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log('Дані форми відправлено:', currentState);
+});
